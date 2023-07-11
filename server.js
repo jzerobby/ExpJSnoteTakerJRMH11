ddc951f5a0b26f7283db38317b68b0fca2847f5f
JZERO MAKILING <jzerorobby07092016@gmail.com>
2023-07-10 19:00:09 -0700

    modified files

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(express.static('./public'));
